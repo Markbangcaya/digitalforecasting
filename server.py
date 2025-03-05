5668f164ca896f345f5d4ef3e83d101547409794
Markbangcaya <bangcaya.markcharlton@dnsc.edu.ph>
2025-03-06 04:51:04 +0800

    Update server.py

--- server.py
+++ server.py
@@ -1,22 +1,22 @@
-import os
-import http.server
-import socketserver
+# import os
+# import http.server
+# import socketserver
 
-from http import HTTPStatus
-
-
-class Handler(http.server.SimpleHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(HTTPStatus.OK)
-        self.end_headers()
-        msg = 'Hello! you requested %s' % (self.path)
-        self.wfile.write(msg.encode())
+# from http import HTTPStatus
 
 
-port = int(os.getenv('PORT', 80))
-print('Listening on port %s' % (port))
-httpd = socketserver.TCPServer(('', port), Handler)
-httpd.serve_forever()
+# class Handler(http.server.SimpleHTTPRequestHandler):
+#     def do_GET(self):
+#         self.send_response(HTTPStatus.OK)
+#         self.end_headers()
+#         msg = 'Hello! you requested %s' % (self.path)
+#         self.wfile.write(msg.encode())
+
+
+# port = int(os.getenv('PORT', 80))
+# print('Listening on port %s' % (port))
+# httpd = socketserver.TCPServer(('', port), Handler)
+# httpd.serve_forever()
 
 # pip install Flask
 # pip install matplotlib
